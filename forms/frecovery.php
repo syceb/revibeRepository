2736beb864a7f0bbb5055d18026c21c81afa82e2
Anninenn <anni.saarinen88@gmail.com>
2021-04-20 22:18:10 +0300

    Anninenn

--- forms/frecovery.php
+++ forms/frecovery.php
@@ -4,6 +4,7 @@
 <title>Page Title</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="css/projekti.css">
 </head>
 
@@ -15,11 +16,15 @@
 
   <article>
     <h1>Stressin seuranta</h1>
+    <input class="heart" type="image" src="images/sydan2.png" name="submit" width="50px" height="50px" alt="submit"/>  
     <input type="button" onclick="location.href='stressMonitoring.php';" value="Mittaa syke" />
   </article>
 
   <article>
     <h1>Unen seuranta</h1>
+
+    <i class="fa fa-clock-o"></i>
+    <br>
     <input type="button" onclick="location.href='sleepMonitoring.php';" value="Lisää unitiedot" />
   </article>
 
