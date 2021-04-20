0b23af818438888a82a066f4146095fd18a3d9b8
syceb <74966877+syceb@users.noreply.github.com>
2021-04-20 20:51:39 +0300

    apin korjaus

--- api/api.php
+++ api/api.php
@@ -4,19 +4,19 @@ header("Content-Type: application/json; charset=UTF-8");
 
 
 $data = array(
-    array("year"=>2010,
+    array("year"=>"2010",
     "value"=>1),
-    array("year"=>2011,
+    array("year"=>"2011",
     "value"=>2),
-    array("year"=>2012,
+    array("year"=>"2012",
     "value"=>3),
-    array("year"=>2013,
+    array("year"=>"2013",
     "value"=>5),
-    array("year"=>2014,
+    array("year"=>"2014",
     "value"=>1),
-    array("year"=>2015,
+    array("year"=>"2015",
     "value"=>4),
-    array("year"=>2016,
+    array("year"=>"2016",
     "value"=>3)
 );
 echo(json_encode($data));
