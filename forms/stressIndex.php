fd1f7e7c767b693c432f001291e265255015ea6f
syceb <74966877+syceb@users.noreply.github.com>
2021-04-22 19:31:41 +0300

    päiväkirja

---
+++ forms/stressIndex.php
@@ -0,0 +1,22 @@
+
+<form method="post">
+  <p>
+   
+  <br />  
+  <textarea rows = "4" cols = "50" name = "givenDescription"></textarea>
+  </p><p>
+  
+  
+  </p><p>Stressi tänään
+  <select name="givenPain">
+  <option value="1">Ei stressiä</option>
+  <option value="2">Kohtalaisesti stressiä</option>
+  <option value="3">Ny ressaa</option>
+  <option value="4">Paljon stressiä</option>
+  <option value="5">En voi hyvin</option>
+</select>
+  </p><p>
+  <br />  <input type="submit" name="submitRecovery" value="Tallenna"/>
+          
+   </p>
+</form>
--- stressMonitoring.php
+++ stressMonitoring.php
@@ -1,44 +1,59 @@
 <?php include("includes/iheader.php");?>
 <?php include("includes/burgermenu.php");?>
+<?php include("forms/stressIndex.php");?>
 
 
 
 
 
 
-
-<h1>Stressin seuranta</h1>
-
-<article>
-<label for="appt">Mittaa syke</label>
-<br>
-
-  <label for="appt">Kuinka voit:</label>
-  <br>
-  <select id="gender" name="givenGender">
-      <option value="Ei stressiä">Ei stressiä</option>
-      <option value="Vähän stressiä">Vähän stressiä</option>
-      <option value="Kohtalaisesti stressiä">Kohtalaisesti stressiä</option>
-      <option value="Paljon stressiä">Paljon stressiä</option>
-      <option value="En voi hyvin">En voi hyvin</option>
-    </select>
-
-    <p>
-      <input id="tallenna" type="submit" name="submitFeeling" value="Tallenna"/>
-   
-  </p>
-
- 
-</article>
-
-
-</body>
 
 <?php
-//Lomakkeen submit painettu???
-if(isset($_POST['submitFeeling'])){}
+ //kirjautuneen käyttäjän userID?
+    $data1['email'] = $_SESSION['suserEmail'];
+    //var_dump($data1);
+    $sql1 = "SELECT userID FROM wsk6_user where userEmail =  :email";
+    $kysely1=$DBH->prepare($sql1);
+    $kysely1->execute($data1);
+    $tulos1=$kysely1->fetch();
+    $currentUserID=$tulos1[0];
+    ?>
+<?php
+if(isset($_POST['submitRecovery'])){
+   try {
+    $data2['commentText']=$_POST['givenDescription'];
+    $data2['commentPainLevel']=$_POST['givenPain'];
+    $data2['commentUserID']=$currentUserID;
+    //var_dump($data2);
+    $sql2="INSERT INTO wsk6_comment (commentText, commentPainLevel, commentUserID)
+    VALUES (:commentText, :commentPainLevel, :commentUserID);";
+     $kysely2 = $DBH->prepare($sql2); 
+     $kysely2->execute($data2);
+   } catch(PDOException $e) {
+    file_put_contents('log/DBErrors.txt', 'index.php: '.$e->getMessage()."\n", FILE_APPEND);
+   }
+  }
 ?>
+  <h2>Stressipäiväkirja</h2>
+<?php
+$data3['commentUserID'] = $currentUserID;
+$sql3 = "SELECT commentText, commentPainLevel, commentDate FROM wsk6_comment WHERE commentUserID = :commentUserID ORDER BY commentDate DESC LIMIT 30";
+$kysely3=$DBH->prepare($sql3);
+$kysely3->execute($data3);				
 
-</html>
+	echo("<table>
+	 	<tr>
+			<th>Merkintä</th>
+      <th>Stressitaso (1-5)</th>
+      <th>Päivämäärä</th>
+		</tr>");
+	
+		while	($row=$kysely3->fetch()){	
+				echo("<tr><td>".$row["commentText"]."</td>
+				<td>".$row["commentPainLevel"]."</td>
+				<td>".$row["commentDate"]."</td></tr>");
+		}
+	
+  echo("</table>");?>
 
 <?php include("includes/ifooter.php");?>
