b17e051a4977edd9758b07f5056cb217bd09ed36
syceb <74966877+syceb@users.noreply.github.com>
2021-04-28 13:25:35 +0300

    typo korjaus

--- userProfile.php
+++ userProfile.php
@@ -1,7 +1,7 @@
 <?php include("includes/iheader.php");?>
 <?php include("includes/burgermenu.php");?>
 <?php
-//Määritetään nykeisen käyttäjän email ja etunimi asd1 ja asd2 arvoiksi.
+//Mï¿½ï¿½ritetï¿½ï¿½n nykeisen kï¿½yttï¿½jï¿½n email ja etunimi asd1 ja asd2 arvoiksi.
 $asd1=$_SESSION['suserEmail'];
 $asd2=$_SESSION['suserName'];
 ?>
@@ -16,15 +16,15 @@ if ($conn->connect_error) {
 //Haetaan tiedot kaikilta joilla on sama etunimi ja email kuin asd1 ja asd2.
 $sql = "SELECT userID, userName, userSurname, userEmail, userAge, userGender FROM revibe_user WHERE userEmail = '$asd1' AND userName = '$asd2'";
 $result = $conn->query($sql);
-//Tarkistetaan onko tietoja enemmän kuin 0 ja tulostetaan ne.
+//Tarkistetaan onko tietoja enemmï¿½n kuin 0 ja tulostetaan ne.
 if ($result->num_rows > 0) {
   while($row = $result->fetch_assoc()) {
     echo "Nimi: " . $row["userName"]. " " . $row["userSurname"]. " - Email: " . $row["userEmail"]. 
-    " - Ikä: " . $row["userAge"]. " - Sukupuoli: " . $row["userGender"]."<br>";
+    " - Ikï¿½: " . $row["userAge"]. " - Sukupuoli: " . $row["userGender"]."<br>";
   }
-  //Jos tietoja ei ole tulostetaan "Ei profiili tietoja".
+  //Jos tietoja ei ole tulostetaan "Ei profiilitietoja".
 } else {
-  echo "Ei profiili tietoja";
+  echo "Ei profiilitietoja";
 }
 //katkaistaan database yhteys.
 $conn->close();
