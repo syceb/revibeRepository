2e3b1d7edab3d26d3200dec64a1f64ed78d223ad
maaab <julius.marttunen@hotmail.com>
2021-04-29 13:48:57 +0300

    aua

---
+++ js/sleepTime.js
@@ -0,0 +1,21 @@
+document.getElementById("submitTime").onclick = timeCalc;
+
+
+
+function timeCalc() {
+
+    let sleep = new Date().toDateString("yyyy-MM-dd") + " " + document.getElementById("sleep").value;
+    let awake = new Date().toDateString("yyyy-MM-dd") + " " + document.getElementById("awake").value;
+    
+    
+    if(sleep < awake){
+        let diff = (new Date(awake) - new Date(sleep)) / 60/ 60 / 1000;
+        document.getElementById("diff").innerHTML = (diff);
+        console.log(diff);}
+
+    else{ 
+    let diff = (24 - new Date(sleep) / 60/ 60 / 1000 + new Date(awake)/ 60/ 60 / 1000);
+    document.getElementById("diff").innerHTML = (diff);
+    console.log(diff);
+    }
+    }
--- sleepMonitoring.php
+++ sleepMonitoring.php
@@ -13,13 +13,48 @@
     <label for="awake">Heräämisaika:</label>
     <input type="time" id="awake" name="awake" min="00:00" max="24:00" required>
 
-    <input type="button" value="Tallenna" onclick="msg()">
+    <input type="button" value="Tallenna" id = "submitTime" name="submitTime" onclick="msg()">
 
     <p> Tyypillisesti aikuisen ihmisen unentarve on noin 7–9 tuntia vuorokaudessa.</p>
+    <p>Total: <br /> <span name="sleepTime" id="diff"></span>&nbsp; hours.</p>
 
  </article>
 
+ <script src="js/sleepTime.js"></script>
+ 
 
 
 
-<?php include("includes/ifooter.php");?>
+ <?php
+ //kirjautuneen käyttäjän userID?
+    $data1['email'] = $_SESSION['suserEmail'];
+    //var_dump($data1);
+    $sql1 = "SELECT userID FROM revibe_user where userEmail =  :email";
+    $kysely1=$DBH->prepare($sql1);
+    $kysely1->execute($data1);
+    $tulos1=$kysely1->fetch();
+    $currentUserID=$tulos1[0];
+    ?>
+
+<?php
+
+
+
+if(isset($_POST['submitTime'])){
+   try {
+    $data3['commentSleep']=$_POST['sleepTime'];
+    
+    $data3['commentUserID']=$currentUserID;
+    //var_dump($data2);
+    $sql3="INSERT INTO revibe_sleepTime (commentSleep, commentUserID)
+    VALUES (:commentSleep, :commentUserID);";
+     $kysely3 = $DBH->prepare($sql3); 
+     $kysely3->execute($data3);
+   } catch(PDOException $e) {
+    file_put_contents('log/DBErrors.txt', 'index.php: '.$e->getMessage()."\n", FILE_APPEND);
+   }
+  }
+?>
+<?php
+
+include("includes/ifooter.php");?>
