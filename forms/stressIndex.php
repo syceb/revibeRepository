e9eeae02bbdb292f0a380916e3129f1b42800ed2
syceb <74966877+syceb@users.noreply.github.com>
2021-04-29 15:16:57 +0300

    css

--- forms/stressIndex.php
+++ forms/stressIndex.php
@@ -5,12 +5,12 @@
   <p>
    
   <br />  
-  <textarea class="center" rows = "4" cols = "50" placeholder = "Mikä stressaa?" name = "givenDescription"></textarea>
+  <textarea class="stressMonitoring" rows = "4" cols = "50" placeholder = "Miten voit?" name = "givenDescription"></textarea>
   </p><p>
   
   
-  </p><p><h1 class="center">Stressi tänään</h1>
-  <select class="center" name="givenPain">
+  </p><p><h1 class="stresstoday">Stressi tänään</h1>
+  <select class="indexi" name="givenPain">
   <option value="1">Ei stressiä</option>
   <option value="2">Kohtalaisesti stressiä</option>
   <option value="3">Ny ressaa</option>
@@ -18,7 +18,7 @@
   <option value="5">En voi hyvin</option>
 </select>
   </p><p>
-  <br />  <input class="center" type="submit" name="submitRecovery" value="Tallenna"/>
+  <br />  <input id="tallenna" type="submit" name="submitRecovery" value="Tallenna"/>
           
    </p>
    <br>
