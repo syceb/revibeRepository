60f44183b07cfac25f2fda70fc4a8e56fa44e1bb
rikulah <33826662+rikulah@users.noreply.github.com>
2021-04-30 15:09:24 +0300

    oba oba

--- userProfile.php
+++ userProfile.php
@@ -1,33 +1,39 @@
 <?php include("includes/iheader.php");?>
 <?php include("includes/burgermenu.php");?>
 <?php
-//Mï¿½ï¿½ritetï¿½ï¿½n nykeisen kï¿½yttï¿½jï¿½n email ja etunimi asd1 ja asd2 arvoiksi.
+//Määritetään nykeisen käyttäjän email ja etunimi asd1 ja asd2 arvoiksi.
 $asd1=$_SESSION['suserEmail'];
 $asd2=$_SESSION['suserName'];
+$asd3=$_SESSION['suserID'];
 ?>
 <body>
  <?php
 //Luodaan yhteys databaseen.
-$conn = new mysqli('mysql.metropolia.fi', 'miikkast', 'Miikkantoivonen', 'miikkast');
+$conn =mysqli_connect('mysql.metropolia.fi', 'miikkast', 'Miikkantoivonen', 'miikkast');
 //Tarkastetaan yhteys.
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 //Haetaan tiedot kaikilta joilla on sama etunimi ja email kuin asd1 ja asd2.
-$sql = "SELECT userID, userName, userSurname, userEmail, userAge, userGender FROM revibe_user WHERE userEmail = '$asd1' AND userName = '$asd2'";
+$sql = "SELECT userID, userName, userSurname, userEmail, userAge, userGender FROM revibe_user WHERE userID = '$asd3'";
 $result = $conn->query($sql);
-//Tarkistetaan onko tietoja enemmï¿½n kuin 0 ja tulostetaan ne.
+//Tarkistetaan onko tietoja enemmän kuin 0 ja tulostetaan ne.
 if ($result->num_rows > 0) {
   while($row = $result->fetch_assoc()) {
-    echo "Nimi: " . $row["userName"]. " " . $row["userSurname"]. " - Email: " . $row["userEmail"]. 
-    " - Ikï¿½: " . $row["userAge"]. " - Sukupuoli: " . $row["userGender"]."<br>";
+    echo "ID: " . $row["userID"]. "Nimi: " . $row["userName"]. " " . $row["userSurname"]. " - Email: " . $row["userEmail"]. 
+    " - Ikä: " . $row["userAge"]. " - Sukupuoli: " . $row["userGender"]."<br>";
   }
-  //Jos tietoja ei ole tulostetaan "Ei profiilitietoja".
+  //Jos tietoja ei ole tulostetaan "Ei profiili tietoja".
 } else {
-  echo "Ei profiilitietoja";
+  echo "Ei profiili tietoja";
 }
 //katkaistaan database yhteys.
 $conn->close();
 ?>
+
+<input class="testi" onclick="location.href='editProfile.php'"; value="Muokkaa tietoja">
+<input class="testi1" onclick="location.href='editPassword.php'"; value="Vaihda salasana">
+<input class="testi2" onclick="location.href='removeProfile.php'"; value="Poista profiili">
+
 </body>
 <?php include("includes/ifooter.php");?>
